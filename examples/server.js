9ad258d2d6f44131091cf5f7e4a4c16806290083
Jon Buckley <jon@jbuckley.ca>
2012-10-22 15:38:43 -0400

    Improve the example server

--- examples/server.js
+++ examples/server.js
@@ -1,16 +1,21 @@
-var express = require("express");
+var express = require("express"),
+    app = express.createServer();
 
-var app = express.createServer();
-
+// The bodyParser, cookieParser, and session middlewares are required for express-persona
+// If you don't want to use express' session middle, you could also use `client-sessions`
 app.use(express.logger())
    .use(express.static(__dirname))
+   .use(express.directory(__dirname))
    .use(express.bodyParser())
    .use(express.cookieParser())
    .use(express.session({
      secret: "mozillapersona"
    }));
 
-require("../../index.js")(app, {
+// This is how you include express-persona in your app
+// In your own app you should use `require("express-persona")`
+// You *must* specify the audience option
+require("../index.js")(app, {
   audience: "http://localhost:3000"
 });
 
